d9da56a2248f6aaeb9b279c1976d188504416292
Gianni <giovannivaccarino03@gmail.com>
2024-03-27 17:02:18 +0100

    Writing csv

--- task3.py
+++ task3.py
@@ -1,1 +1,9 @@
-print("ciao")
+import csv
+
+
+with open('FaultList.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for i in range (5):
+        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+        spamwriter.writerow(['x', 'Lovely Spam', 'Wonderful Spam'])
