980551f63cdbc0416a1603f182d35f677938eb33
nicolovacis <nicolo.vacis@gmail.com>
2024-03-27 19:01:57 +0100

    Aggiornamento Fault List

--- task3.py
+++ task3.py
@@ -1,8 +1,21 @@
 import csv
-
+import random
 
 with open('FaultList.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for i in range (5):
-        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+
+    i=0
+
+    for k in weights.keys():
+
+        if k.endswith(".weight"):
+            tensorWeights = weights.get(k)
+            l=random.randint(1,5)
+            for j in range(l):
+                x=random.randint(0, len(tensorWeights)-1)
+                y=random.randint(0, len(tensorWeights[x])-1)
+                z=random.randint(0, len(tensorWeights[x][y])-1)
+                w=random.randint(0, len(tensorWeights[x][y][z])-1)
+            spamwriter.writerow([i] + [k] + ["([x],[y],[z],[w])"], ...)
+            i+=1
