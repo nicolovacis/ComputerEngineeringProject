82d6a444258b6faf020f923a8afc3be6b068c138
Gianni <giovannivaccarino03@gmail.com>
2024-04-03 20:13:48 +0200

    update

--- task3.py
+++ task3.py
@@ -8,29 +8,20 @@ with open('FaultList.csv', 'w', newline='') as csvfile:
 
     i=0
 
-    for k in weights.keys():
+    for k in range(10):#ipotizzo 10 layer coi pesi
+        l = random.randint(1, 5)
+        for j in range(l):
+            # OTTENGO IL TENSORE IN FORMATO LISTA FORMATA DA INT --> [4,2,3,5]
+            #tensorWeights_size_list = list(tensorWeights.size())
+            tensorWeights_size_list = [768, 10, 97]
+            # OGNI TENSORE PUO' AVERE DIM DIVERSA --> CALCOLO IN MANIERA DINAMICA/RANDOMICA L'ELEMENTO SINGOLO DEL TENSORE
+            listOut = []
 
-        # ITERO SU TUTTE LE CHIAVI/LAYER CHE TERMINANO CON WEIGHT - sono dei pesi
-        if k.endswith(".weight"):
+            for dimTensor in tensorWeights_size_list:
+                x = random.randint(0, dimTensor - 1)
+                listOut.append(x)
 
-            # PER OGNI LAYER OTTENGO I TENSORI ASSOCIATI
-            tensorWeights = weights.get(k)
-
-            # ITERO SU OGNI LAYER IN MANIERA RANDOMICA TRA a E b
-            l=random.randint(1,5)
-            for j in range(l):
-
-                # OTTENGO IL TENSORE IN FORMATO LISTA FORMATA DA INT --> [4,2,3,5]
-                tensorWeights_size_list=list(tensorWeights.size())
-
-                # OGNI TENSORE PUO' AVERE DIM DIVERSA --> CALCOLO IN MANIERA DINAMICA/RANDOMICA L'ELEMENTO SINGOLO DEL TENSORE
-                list=[]
-
-                for t in len(tensorWeights_size_list):
-                    x=random.randint(0, tensorWeights_size_list[t]-1)
-                    list.append(x)
-
-                # AGGIUNGO LA RIGA AL CSV
-                spamwriter.writerow([i] + [k] + [list] + [...]) #aggiungere bit da flippare
-
-            i+=1
+            bit = "bit"
+            # AGGIUNGO LA RIGA AL CSV
+            spamwriter.writerow([i] + [k] + [listOut] + [bit])  # aggiungere bit da flippare
+            i += 1
