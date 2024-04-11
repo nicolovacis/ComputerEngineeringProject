2b9ace5ada06b0af82461939951447578182dee6
Gianni <giovannivaccarino03@gmail.com>
2024-04-11 15:00:26 +0200

    Bug fixing

--- GenerateFaultList.py
+++ GenerateFaultList.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import math
 from functools import reduce
 
 totalInjections = 10000
@@ -41,17 +42,20 @@ with open('FaultList.csv', 'w', newline='') as csvfile:
             result = reduce(lambda x, y: x * y, tensorWeightsSizeList)
 
             # GETTING HOW MANY INJECTIONS SHOULD I DO ON THE CURRENT LAYER
-            numLayerInjections = (result / totalWeights) * totalInjections
-            rowTensorList = []
+            numLayerInjections = math.ceil((result / totalWeights) * totalInjections)
+            print(numLayerInjections)
 
             for currentNumLayerInjection in range(numLayerInjections):
+                rowTensorList = []
 
                 for tensorDim in tensorWeightsSizeList:
                     x = random.randint(0, tensorDim - 1)
                     rowTensorList.append(x)
 
-                    bit = random.randint(0, 31)
+                bit = random.randint(0, 31)
+                # CONVERTING THE LIST INTO A STRING
+                rowTensorListStr = ' '.join(map(str, rowTensorList))
 
-                    # WRITING A NEW ROW IN THE FAULT_LIST CSV FILE
-                    spamwriter.writerow([globalInjectionCounter] + [layer] + [rowTensorList] + [bit])
-                    globalInjectionCounter += 1
+                # WRITING A NEW ROW IN THE FAULT_LIST CSV FILE
+                spamwriter.writerow([globalInjectionCounter] + [layer] + [rowTensorListStr] + [bit])
+                globalInjectionCounter += 1
