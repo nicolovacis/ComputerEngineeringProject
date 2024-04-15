7616a2791823a3ce4749ace971df4d7310bd7d3e
Gianni <giovannivaccarino03@gmail.com>
2024-04-15 20:07:08 +0200

    Added logic for saving output results

--- FaultInjection.py
+++ FaultInjection.py
@@ -2,14 +2,62 @@
 import csv
 import torch
 
+loader = torch.utils.data.DataLoader(transformed_dataset, batch_size=32, shuffle=False, num_workers=8,
+                                     pin_memory=True)
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def create_linked_list(classesAccuracy):
+    head = None
+
+    # Calculate the accuracy for each class and insert into the linked list
+    for className, result in classesAccuracy.items():
+        classAccuracy = result[1] / result[0]
+        head = insert_node_class(head, className, classAccuracy)
+
+    return head
+
+
+def insert_node_class(head, nameClass, accuracyClass):
+    node = Node((nameClass, accuracyClass))
+
+    if head is None:
+        head = node
+        return head
+
+    current = head
+    prev = None
+
+    while current.next is not None and current.next.data[1] >= accuracyClass:
+        prev = current
+        current = current.next
+
+    if current.data[1] < accuracyClass:
+        if prev is None:
+            node.next = head
+            head = node
+        else:
+            prev.next = node
+            node.next = current
+    else:
+        node.next = current.next
+        current.next = node
+
+    return head
+
 
 def model_execution():
     with torch.no_grad():
-        loader = torch.utils.data.DataLoader(transformed_dataset, batch_size=32, shuffle=False, num_workers=8,
-                                             pin_memory=True)
-
         total_count = 0
         accurate_count = 0
+        classesAccuracy = {
+            "cane": [1, 1]
+        }
         # outputTensors = [] -> nel caso volessimo salvare per ogni inferenza l'output delle predictions
         for images, labels in loader:
             images = images.to(device)  # Size (batch_size=32, 3, 224, 224)
@@ -23,21 +71,29 @@ def model_execution():
 
             accurate_predictions = pred_classes == labels  # Size (batch_size=32,1) of booleans
 
-            # Accumulate the number of accurate prediction and total predictions
-            accurate_count += accurate_predictions.sum().item()
-            total_count += len(accurate_predictions)
+            # calculating accuracy for each class
 
-        # Calculate the accuracy as the proportion of accurate predictions over total predictions
-        accuracy = accurate_count / total_count
+            for index, resultPrediction in enumerate(accurate_predictions):
 
-        return accuracy
+                className = dataset.classes[labels[index]]
+
+                if className in classesAccuracy:
+                    if resultPrediction:
+                        classesAccuracy[className] = [classesAccuracy[className][0],
+                                                      classesAccuracy[className][0][1] + 1]
+                else:
+                    if resultPrediction:
+                        classesAccuracy[className] = [1, 1]
+                    else:
+                        classesAccuracy[className] = [1, 0]
+
+        head = create_linked_list(classesAccuracy)
+
+        return head
 
 
-def output_file_write(outputDict):
-    outputFile = open("InjectionResults.txt", "w")
-    for key in outputDict.keys():
-        outputFile.write(key + outputDict[key])
-    outputFile.close()
+def calculate_output_row(outputGoldModel, outputModel):
+    return
 
 
 def update_weights(floatWeights, weightToChange, bitToChange):
@@ -52,46 +108,49 @@ def update_weights(floatWeights, weightToChange, bitToChange):
     return updatedWeights
 
 
-# -
-
-WEIGHTS_PATH = '/content/drive/MyDrive/Colab Notebooks/vit_iiipet_train_best.pth'
+WEIGHTS_PATH = 'vit_iiipet_train_best.pth'
 weights = torch.load(WEIGHTS_PATH)['model'].state_dict()
 
-with open('FaultList.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar=' ')
-    outputDict = {}  # Matching every injectionNumber to its evaluation result
+with open('FaultListInjection.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
 
-    # FOR EACH ROW IN THE CSV FILE
-    for row in spamreader:
-        # FAULT INJECTION
+    with open('FaultList.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar=' ')
 
-        injectionNumber = row[0]
-        layerInjected = row[1] + ".weight"
-        weightToChange = row[2]
-        bitToChange = row[3]
-
-        convWeights = weights.get(layerInjected)
-        copyWeights = convWeights.clone()  # COPY OF THE ORIGINAL TENSOR ASSOCIATED TO layerInjected
-
-        faultInjectedWeights = update_weights(convWeights, weightToChange, bitToChange)
-
-        with torch.no_grad():
-            convWeights.copy_(faultInjectedWeights)
-            weights[layerInjected] = convWeights
-
+        # EXECUTING THE GOLD MODEL
         model.load_state_dict(weights)
+        outputGoldModel = model_execution()
 
-        # INFERENCES EXECUTION
-        outputModel = model_execution()
+        # FOR EACH INJECTION
+        for injection in spamreader:
+            injectionNumber = injection[0]
+            layerInjected = injection[1] + ".weight"
+            weightToChange = injection[2]
+            bitToChange = injection[3]
+            convWeights = weights.get(layerInjected)
+            copyWeights = convWeights.clone()  # COPY OF THE ORIGINAL TENSOR ASSOCIATED TO layerInjected
 
-        outputDict[injectionNumber] = outputModel
+            faultInjectedWeights = update_weights(convWeights, weightToChange, bitToChange)
 
-        # ROLLING BACK TO THE ORIGINAL TENSOR IN ORDER TO AVOID MULTIPLE INJECTIONS -- do xor again
-        with torch.no_grad():
-            weights[layerInjected] = copyWeights
+            with torch.no_grad():
+                convWeights.copy_(faultInjectedWeights)
+                weights[layerInjected] = convWeights
 
-# WRITING outputDict ON A FILE
-output_file_write(outputDict)
+            model.load_state_dict(weights)
+
+            # INFERENCES EXECUTION
+            outputModel = model_execution()
+
+            injectionOutputResults = calculate_output_row(outputGoldModel, outputModel)
+
+            spamwriter.writerow([injectionNumber] + [layerInjected] + [weightToChange] + [bitToChange]
+                                + [injectionOutputResults[0]] + [injectionOutputResults[1]] + [injectionOutputResults[2]]
+                                + [injectionOutputResults[3]] + [injectionOutputResults[4]])
+
+            # ROLLING BACK TO THE ORIGINAL TENSOR IN ORDER TO AVOID MULTIPLE INJECTIONS -- do xor again
+            with torch.no_grad():
+                weights[layerInjected] = copyWeights
+
 
 # masked tutto uguale
 # non_critical top1 uguale, ma il resto del vettore diverso
