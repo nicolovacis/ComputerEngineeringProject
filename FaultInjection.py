1e5e25149926e01916837961e450b256a32b1a5f
Gianni <giovannivaccarino03@gmail.com>
2024-04-18 20:07:36 +0200

    Bug fixing; name converted from camelCase notation to snake_case

--- FaultInjection.py
+++ FaultInjection.py
@@ -23,6 +23,8 @@ class TransformedDataset(torch.utils.data.Dataset):
         return image, label
 
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 DATASET_PATH = 'downloaded_datasets/oxford_iiit_pet'
 
 dataset = torchvision.datasets.OxfordIIITPet(DATASET_PATH, split='test', download=True)
@@ -47,28 +49,26 @@ model.eval()
 
 model.to(device)
 
-totalInjections = 10000
-
 WEIGHTS_PATH = '/content/drive/MyDrive/Colab Notebooks/vit_iiipet_train_best.pth'
 
 weights = torch.load(WEIGHTS_PATH)['model'].state_dict()
 
 
 # END SETTING
-def generate_output_model(classesAccuracy):
-    outputModel = {}
+def generate_output_model(classes_accuracy):
+    output_model = {}
 
     # CALCULATING FOR EACH CLASS THE ACCURACY AND INSERTING IT INTO THE HASHMAP
-    for className, result in classesAccuracy.items():
-        classAccuracy = result[1] / result[0]
-        outputModel[className] = classAccuracy
+    for class_name, result in classes_accuracy.items():
+        class_accuracy = result[1] / result[0]
+        output_model[class_name] = class_accuracy
 
-    return outputModel
+    return output_model
 
 
 def model_execution():
     with torch.no_grad():
-        classesAccuracy = {}
+        classes_accuracy = {}
 
         for images, labels in loader:
             images = images.to(device)
@@ -82,73 +82,73 @@ def model_execution():
             accurate_predictions = pred_classes == labels
 
             # CALCULATING TOTAL_IMAGES AND NUMBER OF CORRECT PREDICTIONS FOR EACH CLASS
-            for index, resultPrediction in enumerate(accurate_predictions):
+            for index, result_prediction in enumerate(accurate_predictions):
 
-                className = dataset.classes[labels[index]]
+                class_name = dataset.classes[labels[index]]
 
-                if className in classesAccuracy:
-                    if resultPrediction:
-                        classesAccuracy[className] = [classesAccuracy[className][0], classesAccuracy[className][1] + 1]
+                if class_name in classes_accuracy:
+                    if result_prediction:
+                        classes_accuracy[class_name] = [classes_accuracy[class_name][0], classes_accuracy[class_name][1] + 1]
                 else:
-                    if resultPrediction:
-                        classesAccuracy[className] = [1, 1]
+                    if result_prediction:
+                        classes_accuracy[class_name] = [1, 1]
                     else:
-                        classesAccuracy[className] = [1, 0]
+                        classes_accuracy[class_name] = [1, 0]
 
-        outputModelAccuracy = generate_output_model(classesAccuracy)
+        output_model_accuracy = generate_output_model(classes_accuracy)
 
-        return outputModelAccuracy
-
-
-def map_to_array(classesAccuracy):
-    classesAccuracyArr = [(className, accuracy) for className, accuracy in classesAccuracy.items()]
-
-    classesSortedAccuracyArr = sorted(classesAccuracyArr, key=lambda x: x[1], reverse=True)
-
-    classesSortedArr = [item[0] for item in classesSortedAccuracyArr]
-
-    return classesSortedArr
+        return output_model_accuracy
 
 
-def calculate_top_one_robust(outputGoldModel, outputModel):
-    minVariation = float("inf")
-    topOneRobustClass = None
+def map_to_array(classes_accuracy):
+    classes_accuracy_arr = [(class_name, accuracy) for class_name, accuracy in classes_accuracy.items()]
 
-    for className in outputGoldModel.keys():
-        variation = outputGoldModel[className] - outputModel[className]
+    classes_sorted_accuracy_arr = sorted(classes_accuracy_arr, key=lambda x: x[1], reverse=True)
 
-        if variation < minVariation:
-            minVariation = variation
-            topOneRobustClass = className
+    classes_sorted_arr = [item[0] for item in classes_sorted_accuracy_arr]
 
-    return topOneRobustClass
+    return classes_sorted_arr
 
 
-def calculate_metrics(outputGoldModelArr, outputModel):
-    outputModelArr = map_to_array(outputModel)
-    topOneCorrect = outputModelArr[0]
+def calculate_top_one_robust(output_gold_model, output_model):
+    min_variation = float("inf")
+    top_one_robust_class = None
 
-    if outputGoldModelArr == outputModelArr:
-        return topOneCorrect, 1, 0, 0
+    for class_name in output_gold_model.keys():
+        variation = output_gold_model[class_name] - output_model[class_name]
 
-    if outputGoldModelArr[0] == outputModelArr[0]:
-        return topOneCorrect, 0, 1, 0
+        if variation < min_variation:
+            min_variation = variation
+            top_one_robust_class = class_name
 
-    return topOneCorrect, 0, 0, 1
+    return top_one_robust_class
 
 
-def update_weights(floatWeights, weightToChange, bitToChange):
-    integerWeights = floatWeights.view(torch.int32)
+def calculate_metrics(output_gold_model_arr, output_model):
+    output_model_arr = map_to_array(output_model)
+    top_one_correct = output_model_arr[0]
 
-    indices = tuple(weightToChange)
+    if output_gold_model_arr == output_model_arr:
+        return top_one_correct, 1, 0, 0
+
+    if output_gold_model_arr[0] == output_model_arr[0]:
+        return top_one_correct, 0, 1, 0
+
+    return top_one_correct, 0, 0, 1
+
+
+def update_weights(float_weights, weight_to_change, bit_to_change):
+    integer_weights = float_weights.view(torch.int32)
+
+    indices = tuple(weight_to_change)
 
     # x << y INSERT x FOLLOWERD BY y TIMES 0
-    bit = 1 << bitToChange
-    integerWeights[indices] ^= bit
+    bit = 1 << bit_to_change
+    integer_weights[indices] ^= bit
 
-    updatedWeights = integerWeights.view(torch.float32)
+    updated_weights = integer_weights.view(torch.float32)
 
-    return updatedWeights
+    return updated_weights
 
 
 with open('FaultListInjection.csv', 'w', newline='') as csvfile:
@@ -159,40 +159,40 @@ with open('FaultListInjection.csv', 'w', newline='') as csvfile:
 
         # EXECUTING THE GOLD MODEL
         model.load_state_dict(weights)
-        outputGoldModel = model_execution()
-        outputGoldModelArr = map_to_array(outputGoldModel)
+        output_gold_model = model_execution()
+        output_gold_model_arr = map_to_array(output_gold_model)
 
         # FOR EACH INJECTION
         for injection in spamreader:
-            injectionNumber = injection[0]
-            layerInjected = injection[1]
-            weightToChangeStr = injection[2]
-            weightToChange = list(map(int, re.split(r'\s+', weightToChangeStr.strip())))
-            bitToChange = int(injection[3])
-            convWeights = weights.get(layerInjected)
+            injection_number = injection[0]
+            layer_injected = injection[1]
+            weight_to_change_str = injection[2]
+            weight_to_change = list(map(int, re.split(r'\s+', weight_to_change_str.strip())))
+            bit_to_change = int(injection[3])
+            conv_weights = weights.get(layer_injected)
 
-            faultInjectedWeights = update_weights(convWeights, weightToChange, bitToChange)
+            fault_injected_weights = update_weights(conv_weights, weight_to_change, bit_to_change)
 
             with torch.no_grad():
-                convWeights.copy_(faultInjectedWeights)
-                weights[layerInjected] = convWeights
+                conv_weights.copy_(fault_injected_weights)
+                weights[layer_injected] = conv_weights
 
             model.load_state_dict(weights)
 
             # INJECTED MODEL EXECUTION
-            outputModel = model_execution()
+            output_model = model_execution()
 
             # METRICS CALCULATION
-            topOneRobust = calculate_top_one_robust(outputGoldModel, outputModel)
+            top_one_robust = calculate_top_one_robust(output_gold_model, output_model)
 
-            topOneCorrect, masked, nonCritical, critical = calculate_metrics(outputGoldModelArr, outputModel)
+            top_one_correct, masked, non_critical, critical = calculate_metrics(output_gold_model_arr, output_model)
 
             # WRITING THE RESULT FOR THE Nth INJECTION
-            spamwriter.writerow([injectionNumber] + [layerInjected] + [weightToChangeStr] + [bitToChange]
-                                + [topOneCorrect] + [topOneRobust] + [masked]
-                                + [nonCritical] + [critical])
+            spamwriter.writerow([injection_number] + [layer_injected] + [weight_to_change_str] + [bit_to_change]
+                                + [top_one_correct] + [top_one_robust] + [masked]
+                                + [non_critical] + [critical])
 
             # ROLLING BACK TO THE ORIGINAL TENSOR IN ORDER TO AVOID MULTIPLE INJECTIONS
-            originalWeights = update_weights(convWeights, weightToChange, bitToChange)
+            original_weights = update_weights(conv_weights, weight_to_change, bit_to_change)
             with torch.no_grad():
-                weights[layerInjected] = originalWeights
+                weights[layer_injected] = original_weights
--- GenerateFaultList.py
+++ GenerateFaultList.py
@@ -4,60 +4,60 @@ import math
 import torch
 from functools import reduce
 
-totalInjections = 10000
+total_injections = 10000
 WEIGHTS_PATH = '/content/drive/MyDrive/Colab Notebooks/vit_iiipet_train_best.pth'
 
 weights = torch.load(WEIGHTS_PATH)['model'].state_dict()
 
 
-def calc_total_weights(dictWeights):
-    weightsNumber = 0
+def calc_total_weights(dict_weights):
+    weights_number = 0
 
-    for layer in dictWeights.keys():
+    for layer in dict_weights.keys():
 
         if layer.endswith(".weight"):
-            tensorWeights = weights.get(layer)
-            tensorWeightsSizeList = list(tensorWeights.size())
+            tensor_weights = weights.get(layer)
+            tensor_weights_size_list = list(tensor_weights.size())
 
             # MULTIPLYING THE ELEMENTS OF THE SIZE LIST
-            result = reduce(lambda x, y: x * y, tensorWeightsSizeList)
+            result = reduce(lambda x, y: x * y, tensor_weights_size_list)
 
-            weightsNumber += result
+            weights_number += result
 
-    return weightsNumber
+    return weights_number
 
 
-totalWeights = calc_total_weights(weights)
+total_weights = calc_total_weights(weights)
 
 with open('FaultList.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
 
-    globalInjectionCounter = 0
+    global_injection_counter = 0
 
     for layer in weights.keys():
 
         if layer.endswith(".weight"):
-            tensorWeights = weights.get(layer)
-            tensorWeightsSizeList = list(tensorWeights.size())
+            tensor_weights = weights.get(layer)
+            tensor_weights_size_list = list(tensor_weights.size())
 
             # MULTIPLYING THE ELEMENTS OF THE SIZE LIST
-            result = reduce(lambda x, y: x * y, tensorWeightsSizeList)
+            result = reduce(lambda x, y: x * y, tensor_weights_size_list)
 
             # GETTING HOW MANY INJECTIONS SHOULD I DO ON THE CURRENT LAYER
-            numLayerInjections = math.ceil((result / totalWeights) * totalInjections)
+            num_layer_injections = math.ceil((result / total_weights) * total_injections)
 
-            for currentNumLayerInjection in range(numLayerInjections):
-                rowTensorList = []
+            for current_num_layer_injection in range(num_layer_injections):
+                row_tensor_list = []
 
-                for tensorDim in tensorWeightsSizeList:
+                for tensorDim in tensor_weights_size_list:
                     x = random.randint(0, tensorDim - 1)
-                    rowTensorList.append(x)
+                    row_tensor_list.append(x)
 
                 bit = random.randint(0, 31)
                 # CONVERTING THE LIST INTO A STRING
-                rowTensorListStr = ' '.join(map(str, rowTensorList))
+                row_tensor_list_str = ' '.join(map(str, row_tensor_list))
 
                 # WRITING A NEW ROW IN THE FAULT_LIST CSV FILE
-                spamwriter.writerow([globalInjectionCounter] + [layer] + [rowTensorListStr] + [bit])
-                globalInjectionCounter += 1
+                spamwriter.writerow([global_injection_counter] + [layer] + [row_tensor_list_str] + [bit])
+                global_injection_counter += 1
