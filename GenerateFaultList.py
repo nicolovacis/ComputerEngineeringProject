c9a9156af2c2b21088e3e741acfbcbfdf218a4ba
Gianni <giovannivaccarino03@gmail.com>
2024-04-21 23:42:37 +0200

    Added argparse and refactoring for GenerateFaultList

--- GenerateFaultList.py
+++ GenerateFaultList.py
@@ -1,13 +1,9 @@
 import csv
+import argparse
 import random
 import math
 import torch
 from functools import reduce
-
-total_injections = 10000
-WEIGHTS_PATH = '/content/drive/MyDrive/Colab Notebooks/vit_iiipet_train_best.pth'
-
-weights = torch.load(WEIGHTS_PATH)['model'].state_dict()
 
 
 def calc_total_weights(dict_weights):
@@ -16,7 +12,7 @@ def calc_total_weights(dict_weights):
     for layer in dict_weights.keys():
 
         if layer.endswith(".weight"):
-            tensor_weights = weights.get(layer)
+            tensor_weights = dict_weights.get(layer)
             tensor_weights_size_list = list(tensor_weights.size())
 
             # MULTIPLYING THE ELEMENTS OF THE SIZE LIST
@@ -27,37 +23,59 @@ def calc_total_weights(dict_weights):
     return weights_number
 
 
-total_weights = calc_total_weights(weights)
+def main():
+    parser = argparse.ArgumentParser(
+        prog='GenerateFaultList',
+        description='Script that generates a fault list',
+    )
 
-with open('FaultList.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',',
-                            quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+    parser.add_argument('--injections_number', type=int, required=True)
+    parser.add_argument('--weights_path', type=str, required=True)
+    parser.add_argument('--csv_output_path', type=str, required=True)
 
-    global_injection_counter = 0
+    args = parser.parse_args()
 
-    for layer in weights.keys():
+    WEIGHTS_PATH = args.weights_path
+    injections_number = args.injections_number
+    csv_output_path = args.csv_output_path
 
-        if layer.endswith(".weight"):
-            tensor_weights = weights.get(layer)
-            tensor_weights_size_list = list(tensor_weights.size())
+    weights = torch.load(WEIGHTS_PATH)['model'].state_dict()
 
-            # MULTIPLYING THE ELEMENTS OF THE SIZE LIST
-            result = reduce(lambda x, y: x * y, tensor_weights_size_list)
+    total_weights = calc_total_weights(weights)
 
-            # GETTING HOW MANY INJECTIONS SHOULD I DO ON THE CURRENT LAYER
-            num_layer_injections = math.ceil((result / total_weights) * total_injections)
+    with open(csv_output_path, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar=' ', quoting=csv.QUOTE_MINIMAL)
 
-            for current_num_layer_injection in range(num_layer_injections):
-                row_tensor_list = []
+        global_injection_counter = 0
 
-                for tensorDim in tensor_weights_size_list:
-                    x = random.randint(0, tensorDim - 1)
-                    row_tensor_list.append(x)
+        for layer in weights.keys():
 
-                bit = random.randint(0, 31)
-                # CONVERTING THE LIST INTO A STRING
-                row_tensor_list_str = ' '.join(map(str, row_tensor_list))
+            if layer.endswith(".weight"):
+                tensor_weights = weights.get(layer)
+                tensor_weights_size_list = list(tensor_weights.size())
 
-                # WRITING A NEW ROW IN THE FAULT_LIST CSV FILE
-                spamwriter.writerow([global_injection_counter] + [layer] + [row_tensor_list_str] + [bit])
-                global_injection_counter += 1
+                # MULTIPLYING THE ELEMENTS OF THE SIZE LIST
+                result = reduce(lambda x, y: x * y, tensor_weights_size_list)
+
+                # GETTING HOW MANY INJECTIONS SHOULD I DO ON THE CURRENT LAYER
+                num_layer_injections = math.ceil((result / total_weights) * injections_number)
+
+                for current_num_layer_injection in range(num_layer_injections):
+                    row_tensor_list = []
+
+                    for tensor_dim in tensor_weights_size_list:
+                        x = random.randint(0, tensor_dim - 1)
+                        row_tensor_list.append(x)
+
+                    bit = random.randint(0, 31)
+                    # CONVERTING THE LIST INTO A STRING
+                    row_tensor_list_str = ' '.join(map(str, row_tensor_list))
+
+                    # WRITING A NEW ROW IN THE FAULT_LIST CSV FILE
+                    spamwriter.writerow([global_injection_counter] + [layer] + [row_tensor_list_str] + [bit])
+                    global_injection_counter += 1
+
+
+if __name__ == '__main__':
+    main()
