c19438884e4e6d4713772c4c38066e44c1ebf84d
nicolovacis <nicolo.vacis@gmail.com>
2024-05-01 22:22:41 +0200

    Graph Analytics - worst critical layers

--- GraphAnalysis.py
+++ GraphAnalysis.py
@@ -12,3 +12,15 @@ grouped_df = df.groupby('layer_injected').agg({
 # Print the grouped DataFrame
 print(grouped_df)
 
+grouped_df.plot(kind='bar', y=['masked'], title='Count of Masked', ylabel='Count', xlabel='Layer', figsize=(10, 6))
+grouped_df.plot(kind='bar', y=['non_critical'], title='Count of Non-Critical', ylabel='Count', xlabel='Layer', figsize=(10, 6))
+grouped_df.plot(kind='bar', y=['critical'], title='Count of Critical', ylabel='Count', xlabel='Layer', figsize=(10, 6))
+
+# Group by 'layer_injected' and aggregate the counts of 'critical'
+grouped_df = df.groupby('layer_injected')['critical'].sum().reset_index()
+
+# Sort the DataFrame by 'critical' column in descending order and select the top 10 layers
+top_10_critical_layers = grouped_df.sort_values(by='critical', ascending=False).head(10)
+
+# Plot the top 10 critical layers
+top_10_critical_layers.plot(kind='bar', x='layer_injected', y='critical', title='Top 10 Critical Layers', ylabel='Count', xlabel='Layer', figsize=(10, 6))
