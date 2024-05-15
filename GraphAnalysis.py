27f96349015196880a9e22be090bc1c681f1059a
nicolovacis <nicolo.vacis@gmail.com>
2024-05-15 12:57:08 +0200

    GraphAnalysis fixed: - no more dim_tensor_correlation - only critical layers for vulnerability

--- GraphAnalysis.py
+++ GraphAnalysis.py
@@ -2,7 +2,6 @@ import pandas as pd
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
 
 def dim_tensor_correlation(file, WEIGHTS_PATH):
     weights = torch.load(WEIGHTS_PATH)['model'].state_dict()
@@ -12,17 +11,16 @@ def dim_tensor_correlation(file, WEIGHTS_PATH):
     # Initialize a dictionary to store critical injections for each dimension
     dimension_critical = {}
 
-
     for layer_name in df['layer_injected'].unique():
 
         # Tensor dimension
         tensorWeights = weights.get(layer_name)
-        tensorWeights_size_list=list(tensorWeights.size()) # list format of the tensor size --> [4,2,3,5]
+        tensorWeights_size_list = list(tensorWeights.size()) # list format of the tensor size --> [4,2,3,5]
         tensor_dim = 1
         for num in tensorWeights_size_list:
             tensor_dim *= num
 
-            # Calculate total injections for the current layer
+        # Calculate total injections for the current layer
         total_injections = df[df['layer_injected'] == layer_name]['masked'].sum() + \
                            df[df['layer_injected'] == layer_name]['non_critical'].sum() + \
                            df[df['layer_injected'] == layer_name]['critical'].sum()
@@ -39,20 +37,17 @@ def dim_tensor_correlation(file, WEIGHTS_PATH):
     # Calculate the mean percentage of critical injections for each dimension
     dimension_mean_critical = {dim: np.mean(perc) for dim, perc in dimension_critical.items()}
 
-    # Plot a graph
-    plt.figure(figsize=(8, 6))
-    plt.scatter(dimension_mean_critical.keys(), dimension_mean_critical.values())
-    plt.xlabel('Dimension of Tensor')
-    plt.ylabel('Percentage of Critical Injections')
-    plt.title('Correlation between Tensor Dimension and Critical Injections')
-    plt.xticks(rotation=45)  # Rotate x-axis labels by 45 degrees
-    plt.grid(True)
-    plt.show()
-
+    # Plot a graph (Commented out)
+    # plt.figure(figsize=(8, 6))
+    # plt.scatter(dimension_mean_critical.keys(), dimension_mean_critical.values())
+    # plt.xlabel('Dimension of Tensor')
+    # plt.ylabel('Percentage of Critical Injections')
+    # plt.title('Correlation between Tensor Dimension and Critical Injections')
+    # plt.xticks(rotation=45)  # Rotate x-axis labels by 45 degrees
+    # plt.grid(True)
+    # plt.show()
 
 def injection_types_plot(file):
-
-    # < PERCENTAGE OF INJECTION TYPES >
 
     df = pd.read_csv(file)
 
@@ -62,26 +57,19 @@ def injection_types_plot(file):
         'critical': 'sum'
     })
 
-    #print(grouped_df)
-
     total_injections = grouped_df['masked'] + grouped_df['non_critical'] + grouped_df['critical']
 
-    grouped_df['masked_percent'] = (grouped_df['masked'] / total_injections) * 100
-    grouped_df['non_critical_percent'] = (grouped_df['non_critical'] / total_injections) * 100
     grouped_df['critical_percent'] = (grouped_df['critical'] / total_injections) * 100
 
     grouped_df.drop(columns=['masked', 'non_critical', 'critical'], inplace=True)
 
-    grouped_df.plot(kind='bar', stacked=True, title='Percentage of Injection Types', ylabel='Percentage', xlabel='Layer', figsize=(10, 6))
-
-
+    grouped_df.plot(kind='bar', y='critical_percent', title='Percentage of Critical Injections', ylabel='Percentage', xlabel='Layer', figsize=(10, 6))
 
 def vulnerability_plot(type, file):
     df = pd.read_csv(file)
 
-    if(type == "layer"):
+    if type == "layer":
 
-        # < VULNERABILITY - layer >
         grouped_df = df.groupby('layer_injected').agg({
             'masked': 'sum',
             'non_critical': 'sum',
@@ -94,11 +82,10 @@ def vulnerability_plot(type, file):
 
         top_critical_layers = grouped_df.sort_values(by='critical_percentage', ascending=False).head(10)
 
-        top_critical_layers.plot(kind='bar', y='critical_percentage', title='Vulnerable layers', ylabel='Critical Injection Percentage (out of all the injections)', xlabel='Layer', figsize=(10, 6))
+        top_critical_layers.plot(kind='bar', y='critical_percentage', title='Vulnerable Layers', ylabel='Critical Injection Percentage (out of all the injections)', xlabel='Layer', figsize=(10, 6))
 
-    elif(type == 'bit'):
+    elif type == 'bit':
 
-        # < VULNERABILITY - bit >
         df['bit_to_change'] = pd.to_numeric(df['bit_to_change'])
 
         grouped_df = df.groupby('bit_to_change').agg({
@@ -123,7 +110,7 @@ def vulnerability_plot(type, file):
 
         grouped_df.drop(columns=['masked', 'non_critical', 'critical'], inplace=True)
 
-        grouped_df.plot(kind='bar', stacked=True, title='Vulnerable bits', ylabel='Critical Injection Percentage (out of all the injections)', xlabel='Bit', figsize=(10, 6))
+        grouped_df.plot(kind='bar', stacked=True, title='Vulnerable Bits', ylabel='Critical Injection Percentage (out of all the injections)', xlabel='Bit', figsize=(10, 6))
 
 
 if __name__ == "__main__":
@@ -145,7 +132,7 @@ if __name__ == "__main__":
     file_path = args.file_path
     weight_path = args.weight_path
 
-    dim_tensor_correlation(file_path, weight_path)
+    #dim_tensor_correlation(file_path, weight_path)
     injection_types_plot(file_path)
     vulnerability_plot("layer", file_path)
     vulnerability_plot("bit", file_path)
